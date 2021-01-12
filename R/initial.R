3be770e57d4557e0cfbb8a4b464d9df0a45ed587
Francis Barton <fbarton@alwaysdata.net>
2021-01-12 12:41:22 +0000

    More progress

--- R/initial.R
+++ R/initial.R
@@ -1,13 +1,13 @@
 
 # libraries ---------------------------------------------------------------
-
-library(here)
-library(dplyr)
-library(jogger)
-library(purrr)
-library(sf)
-library(tmap)
-
+{
+  library(here)
+  library(dplyr)
+  library(jogger)
+  library(purrr)
+  library(sf)
+  library(tmap)
+}
 
 tmap_mode("plot")
 
@@ -94,54 +94,130 @@ tmap_save(sussex5, "sussex5.png")
 
 
 
-choose_grid <- function(areas_df, grid_dims) {
-
-  # areas_df <- areas_df %>%
-  #   dplyr::arrange(shape_area) %>%
-  #   dplyr::select(!dplyr::any_of(c("shape_area", "shape_length")))
+# choose_grid <- function(areas_df, grid_dims) {
+choose_grid <- function(areas_df, cell_size) {
 
   grid <- sf::st_make_grid(
     areas_df,
     what = "polygons",
     square = FALSE,
     flat_topped = TRUE,
-    n = grid_dims
+    # n = grid_dims
+    cellsize = cell_size
   ) %>%
     sf::st_sfc(crs = 7405) %>%
     sf::st_as_sf()
 
-  areas_df %>%
+  results <- areas_df %>%
     split(forcats::fct_inorder(.$lad20nm)) %>%
     purrr::reduce(
-      gather_hexes,
-      .init = list(NULL, grid)
-    ) %>%
+      # gather_hexes,
+      hex_array,
+      .init = list(NULL, grid, NULL, NULL)
+    )
+
+  hexes <- results %>%
     purrr::pluck(1)
+
+  union <- results %>%
+    purrr::pluck(3)
+
+  list(grid, hexes, union)
+}
+
+
+hex_array <- function(input, areas_df, first_pass = TRUE) {
+
+  results <- input[[1]]
+  grid <- input[[2]]
+  prev_union <- input[[3]]
+  prev_results <- input[[4]]
+
+  if (first_pass) {
+    prev_results <- results
+    results <- NULL
+  }
+
+  msoa <- areas_df %>%
+    dplyr::slice(1)
+  msoa_centroid <- sf::st_centroid(msoa)
+  areas_out <- areas_df %>%
+    dplyr::slice(-1)
+  # area_centroids <- sf::st_centroid(areas_out)
+
+
+  grid_centroids <- sf::st_centroid(grid)
+  nearest_index <- sf::st_nearest_feature(msoa_centroid, grid_centroids)
+  nearest_hex <- dplyr::slice(grid, nearest_index)
+  grid <- dplyr::slice(grid, -nearest_index)
+
+
+
+  results <- dplyr::bind_rows(
+    results,
+    dplyr::bind_cols(
+      sf::st_drop_geometry(msoa),
+      hex_geometry = nearest_hex
+    )
+  )
+
+  if (nrow(areas_out) == 0) {
+    results <- dplyr::bind_rows(prev_results, sf::st_sf(results))
+    union <- purrr::reduce(list(x = prev_union, sf::st_union(results)), dplyr::bind_rows)
+    list(results, grid, union, NULL)
+  } else {
+    hex_array(list(results, grid, prev_union, prev_results), areas_out, first_pass = FALSE)
+  }
+
 
 }
 
-gather_hexes <- function(input, areas_df, n = 1) {
+
+
+
+gather_hexes <- function(input, areas_df, n = NULL) {
 
   results <- input[[1]]
   grid <- input[[2]]
 
+  area_centroids <- sf::st_centroid(areas_df)
+
+  if (is.null(n)) {
+
+    n <- sf::st_nearest_feature(
+      sf::st_centroid(sf::st_union(areas_df)),
+      area_centroids
+    )
+
+  }
+
+
+
   msoa <- areas_df %>%
     dplyr::slice(n)
-  areas_df <- areas_df %>%
-    dplyr::slice(-n)
   msoa_centroid <- sf::st_centroid(msoa)
+  areas_out <- areas_df %>%
+    dplyr::slice(-n)
+  area_centroids <- sf::st_centroid(areas_out)
 
 
+
+  if (!is.null(results) && any(sf::st_touches(x = grid, y = results, sparse = FALSE)[, 1])) {
+  filter_grid <- grid %>%
+    dplyr::filter(sf::st_touches(x = ., y = results, sparse = FALSE)[, 1])
+  remainder <- grid %>%
+    dplyr::filter(!sf::st_touches(x = ., y = results, sparse = FALSE)[, 1])
+  grid_centroids <- sf::st_centroid(filter_grid)
+  nearest_index <- sf::st_nearest_feature(msoa_centroid, grid_centroids)
+  nearest_hex <- dplyr::slice(filter_grid, nearest_index)
+  filter_grid <- dplyr::slice(filter_grid, -nearest_index)
+  grid <- dplyr::bind_rows(filter_grid, remainder)
+} else {
   grid_centroids <- sf::st_centroid(grid)
-
-
   nearest_index <- sf::st_nearest_feature(msoa_centroid, grid_centroids)
   nearest_hex <- dplyr::slice(grid, nearest_index)
-
   grid <- dplyr::slice(grid, -nearest_index)
-
-  area_centroids <- sf::st_centroid(areas_df)
-  next_area_index <- sf::st_nearest_feature(msoa_centroid, area_centroids)
+}
 
 
   results <- dplyr::bind_rows(
@@ -153,57 +229,42 @@ gather_hexes <- function(input, areas_df, n = 1) {
   ) %>%
     sf::st_sf()
 
-  if (nrow(areas_df) == 0) {
+
+
+  next_area_index <- sf::st_nearest_feature(msoa_centroid, area_centroids)
+
+
+
+  if (nrow(areas_out) == 0) {
     list(results, grid)
   } else {
-
     # gather_hexes(results, areas_df, grid, n = (n + 1))
-    gather_hexes(list(results, grid), areas_df, n = next_area_index)
+    gather_hexes(input = list(results, grid), areas_df = areas_out, n = next_area_index)
   }
 }
 
 
-out <- choose_grid(sussex_bounds, 36)
 
-hex_out <- out %>%
-  dplyr::mutate(msoa11no = stringr::str_extract(msoa11nm, "[0-9]{3}$")) %>%
-  dplyr::mutate(msoa11hclnm = stringr::str_extract(msoa11hclnm, "^.{3}")) %>%
-  dplyr::mutate(fill_col = dplyr::case_when(
-    utla20nm == "Brighton and Hove" ~ "cadetblue2",
-    utla20nm == "West Sussex" ~ "darkorchid4",
-    utla20nm == "East Sussex" ~ "limegreen",
-    TRUE ~ "grey50"
-  ))
+out <- choose_grid(sussex_bounds, cell_size = 3600)
 
-tm_shape(grid) +
+sussex_hexmap23 <- tm_shape(out[[1]]) +
   tm_borders("olivedrab4", lwd = 1) +
-  tm_shape(hex_out) +
-  tm_borders("grey65") +
-  tm_fill("lad20nm", alpha = 1, palette = "Set3") +
+  tm_fill("grey95") +
+  tm_shape(out[[2]] %>%
+      dplyr::mutate(msoa11no = stringr::str_extract(msoa11nm, "[0-9]{3}$")) %>%
+      dplyr::mutate(msoa11hclnm = stringr::str_extract(msoa11hclnm, "^.{3}"))
+    ) +
+  tm_borders() +
+  tm_fill("lad20nm", alpha = 1, palette = "Paired") +
   tm_text("msoa11hclnm", size = 0.5) +
+  tm_shape(out[[3]]) +
+  tm_borders("grey20", lwd = 2) +
   # tm_text("msoa11no", size = 0.5)
-  tm_layout(title = "MSOAs in Sussex, using hexgrid n = 36",
+  tm_layout(title = "MSOAs in Sussex\ncell size = 3600",
             legend.show = FALSE)
 
-sussex_hexmap11 <- choose_grid(sussex_bounds, 34) %>%
-  dplyr::mutate(msoa11no = stringr::str_extract(msoa11nm, "[0-9]{3}$")) %>%
-  dplyr::mutate(msoa11hclnm = stringr::str_extract(msoa11hclnm, "^.{3}")) %>%
-  dplyr::mutate(fill_col = dplyr::case_when(
-    utla20nm == "Brighton and Hove" ~ "cadetblue2",
-    utla20nm == "West Sussex" ~ "darkorchid4",
-    utla20nm == "East Sussex" ~ "limegreen",
-    TRUE ~ "grey50"
-  )) %>%
-  tm_shape() +
-  tm_borders("grey65") +
-  # tm_fill("grey95") +
-  tm_fill("fill_col", alpha = 0.3) +
-  tm_text("msoa11hclnm", size = 0.5) +
-  # tm_text("msoa11no", size = 0.5)
-  tm_layout(title = "MSOAs in Sussex, using hexgrid n = 34")
 
-
-sussex_hexmap11
+sussex_hexmap23
 
 
 tmap_save(sussex_hexmap1, "sussex_hexmap1.png")
