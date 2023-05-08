098a62e529d295839e1a1e73ddfda51ba998e85a
Fran Barton <francis.barton@nuh.nhs.uk>
2023-05-08 23:47:20 +0100

    Latest targets additions

--- _targets.R
+++ _targets.R
@@ -8,7 +8,7 @@ library(targets)
 # library(tarchetypes) # Load other packages as needed. # nolint
 
 # Set target options:
-tar_option_set( 
+tar_option_set(
   packages = c(
     "boundr",
     "dplyr",
@@ -17,54 +17,87 @@ tar_option_set(
     "sf",
     "tibble",
     "tidyr"
-    ), # packages that your targets need to run
-  format = "rds" # default storage format
-  # Set other options as needed.
+    ),
+  format = "rds"
 )
 
 # tar_make_clustermq() configuration (okay to leave alone):
 options(clustermq.scheduler = "multicore")
 
-# tar_make_future() configuration (okay to leave alone):
-# Install packages {{future}}, {{future.callr}}, and {{future.batchtools}} to allow use_targets() to configure tar_make_future() options.
-
 # Run the R scripts in the R/ folder with your custom functions:
 tar_source()
-# source("other_functions.R") # Source other scripts as needed. # nolint
 
-# Replace the target list below with your own:
 list(
-  tar_target(
-    name = eng_regions,
-    command = boundr::bounds(
-      "rgn", "ctry", "England", lookup_year = "2022", crs = 27700)
+  eng_regions |>
+    tar_target(
+      boundr::bounds("rgn", "ctry", "England",
+                     lookup_year = "2022", crs = 27700)
   ),
-  tar_target(
-    name = lads_by_region,
-    command = get_all_region_lads(eng_regions)  
+
+  lads_by_region |>
+    tar_target(
+      get_all_region_lads(eng_regions)
+      ),
+
+  eng_lad_centroids |>
+    tar_target(
+      get_all_lad_centroids(lads_by_region)
+      ),
+
+  msoas_by_region |>
+    tar_target(
+      get_msoas_by_region(lads_by_region, eng_regions)
+      ),
+
+  msoa_centroids_by_region |>
+    tar_target(
+      get_msoa_centroids_by_rgn(lads_by_region, eng_regions)
+      ),
+
+  msoas_sorted_orig |>
+    tar_target(
+      sort_msoas_by_proximity(
+        msoa_centroids_by_region,
+        eng_lad_centroids,
+        msoas_by_region
+      )
   ),
-  tar_target(
-    name = eng_lad_centroids,
-    command = get_all_lad_centroids(lads_by_region)
-  ),
-  tar_target(
-    name = msoas_by_region,
-    command = get_msoas_by_region(lads_by_region, eng_regions)
-  ),
-  tar_target(
-    name = msoa_centroids_by_region,
-    command = get_msoa_centroids_by_rgn(lads_by_region, eng_regions)
-  ),
-  tar_target(
-    name = msoas_sorted_orig,
-    command = sort_msoas_by_proximity(
-      msoa_centroids_by_region,
-      eng_lad_centroids,
-      msoas_by_region
+
+  base_grid_full |>
+    tar_target(
+      create_base_grid(eng_regions, cell_size = 3160)
+      ),
+
+  london_index |>
+    tar_target(
+      extract_london_index(base_grid_full, eng_regions)
+      ),
+
+  london_orig |>
+    tar_target(
+      base_grid_full[london_index]
+      ),
+
+  london_swole |>
+    tar_target(
+      extract_london(base_grid_full, london_index)
+    ),
+
+  base_grid_orig |>
+    tar_target(
+      base_grid_full[eng_regions] |>
+        fettle_edges() |>
+        sf::st_difference(sf::st_union(london_orig))
+      ),
+
+  london_msoas |>
+    tar_target(
+      msoas_sorted_orig[["London"]]
+    ),
+
+  regional_msoas |>
+    tar_target(
+      msoas_sorted_orig |>
+        purrr::discard_at("London")
     )
-  ),
-  tar_target(
-    name = base_grid_orig,
-    command = create_base_grid(eng_regions, cell_size = 3160)
-  )
 )
