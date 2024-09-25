bae69fad5ff3e9c60373f2d1ce21622732e04293
Fran Barton <francis.barton@nhs.net>
2024-09-25 13:01:40 +0100

    targets improvements

--- _targets.R
+++ _targets.R
@@ -28,76 +28,51 @@ options(clustermq.scheduler = "multicore")
 tar_source()
 
 list(
-  eng_regions |>
-    tar_target(
-      boundr::bounds("rgn", "ctry", "England",
-                     lookup_year = "2022", crs = 27700)
-  ),
+  region_bounds |>
+    tar_target(get_region_bounds()),
 
-  lads_by_region |>
-    tar_target(
-      get_all_region_lads(eng_regions)
-      ),
+  lad_lookup |>
+    tar_target(get_lad_lookup()),
 
-  eng_lad_centroids |>
-    tar_target(
-      get_all_lad_centroids(lads_by_region)
-      ),
+  itl2_lookup |>
+    tar_target(get_itl2_lookup()),
+  
+  msoa_lookup |>
+    tar_target(get_msoa_lookup()),
+  
+  itl2_bounds |>
+    tar_target(get_itl2_bounds(itl2_lookup)),
 
-  msoas_by_region |>
-    tar_target(
-      get_msoas_by_region(lads_by_region, eng_regions)
-      ),
+  lad_bounds |>
+    tar_target(get_lad_bounds(lad_lookup, itl2_lookup)),
 
-  msoa_centroids_by_region |>
-    tar_target(
-      get_msoa_centroids_by_rgn(lads_by_region, eng_regions)
-      ),
+  msoa_bounds |>
+    tar_target(get_msoa_bounds(lad_lookup, itl2_lookup, msoa_lookup)),
 
-  msoas_sorted_orig |>
+  msoa_centroids |>
+    tar_target(get_msoa_centroids(lad_lookup, itl2_lookup, msoa_lookup)),
+
+  sorted_msoa_data |>
     tar_target(
-      sort_msoas_by_proximity(
-        msoa_centroids_by_region,
-        eng_lad_centroids,
-        msoas_by_region
-      )
-  ),
+      create_sorted_msoa_data(msoa_centroids, msoa_bounds, itl2_bounds)
+    ),
 
   base_grid_full |>
-    tar_target(
-      create_base_grid(eng_regions, cell_size = 3160)
-      ),
+    tar_target(create_base_grid(region_bounds, cell_size = 3160)),
 
   london_index |>
-    tar_target(
-      extract_london_index(base_grid_full, eng_regions)
-      ),
+    tar_target(extract_london_index(base_grid_full, region_bounds)),
 
   london_orig |>
-    tar_target(
-      base_grid_full[london_index]
-      ),
+    tar_target(base_grid_full[london_index]),
 
   london_swole |>
-    tar_target(
-      extract_london(base_grid_full, london_index)
-    ),
+    tar_target(extract_london(base_grid_full, london_index)),
 
   base_grid_orig |>
     tar_target(
-      base_grid_full[eng_regions] |>
-        fettle_edges() |>
+      base_grid_full[region_bounds] |>
+        # fettle_edges() |>
         sf::st_difference(sf::st_union(london_orig))
-      ),
-
-  london_msoas |>
-    tar_target(
-      msoas_sorted_orig[["London"]]
-    ),
-
-  regional_msoas |>
-    tar_target(
-      msoas_sorted_orig |>
-        purrr::discard_at("London")
     )
 )
